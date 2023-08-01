f191229df9a3fece2d5d491f3939a7d04e4d1331
michellemwangi01 <michellemwangim@gmail.com>
2023-08-02 01:45:16 +0300

    created details component

--- src/Components/FetchAPIData.js
+++ src/Components/FetchAPIData.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
+import Listing_Details from "./Listing_Details";
 
-export const listingsDataContext = React.createContext();
-export const listingDetailsContext = React.createContext();
+import { createContext } from "react";
+// export const listingsDataContext = React.createContext();
+// export const listingDetailsContext = React.createContext();
 const renderAPIListings = `https://db-qwenuresidentials.onrender.com/listings`;
 const renderAPIListingDetails = `https://db-qwenuresidentials.onrender.com/listingDetails`;
 const localAPIListings = ` http://localhost:4000/listings`;
 const localAPIListingDetails = `http://localhost:4000/listingDetails`;
 const APIGoogleURL = `https://rapidapi.com/apidojo/api/bayut?utm_source=youtube.com%2FJavaScriptMastery&utm_medium=DevRel&utm_campaign=DevRel`;
 
-const FetchAPIData = () => {
+export const listingsDataContext = createContext();
+const FetchAPIData = ({ children }) => {
   const [listingsData, setListingsData] = useState([]);
+  // const [listingData, setListingData] = useState({});
 
   useEffect(() => {
     fetch("http://localhost:3000/listings", {
@@ -24,11 +28,13 @@ const FetchAPIData = () => {
       .then((data) => setListingsData(data));
   }, []);
   // console.log(listingsData);
+  const values = { listingsData };
+  console.log(listingsData);
   return (
     <div>
-      <listingsDataContext.Provider
-        value={listingsData}
-      ></listingsDataContext.Provider>
+      <listingsDataContext.Provider value={values}>
+        {children}
+      </listingsDataContext.Provider>
     </div>
   );
 };
--- src/Components/Listing_Details.js
+++ src/Components/Listing_Details.js
@@ -1,53 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { listingsDataContext } from "./FetchAPIData";
 
 const Listing_Details = () => {
+  const { externalID } = useParams();
   const [images, setImages] = useState([]);
   const [currentImage, setCurrentImage] = useState("");
   const [count, setCount] = useState(0);
   const [listingData, setListingData] = useState({});
+  const { listingsData } = useContext(listingsDataContext);
 
-  // const [
-  //   amenities,
-  //   area,
-  //   baths,
-  //   category,
-  //   description,
-  //   location,
-  //   price,
-  //   purpose,
-  //   floorPlan,
-  // ] = listingData;
-
-  // console.log(images.length);
-
+  // console.log(listingsData);
+  // console.log(externalID);
+  const currentProperty = listingsData.find(
+    (property) => property.externalID === externalID
+  );
+  console.log(currentProperty);
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prevCount) => (prevCount + 1) % images.length);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, [images]);
-
-  useEffect(() => {
-    setCurrentImage(images[count]);
-  }, [count]);
-
-  // console.log(count);
-
-  useEffect(() => {
-    fetch(`http://localhost:3000/listingDetails`, {
-      method: "GET",
-      // headers: {
-      //   "X-RapidAPI-Key": "47f6ed740fmsh71585dcfcf20c8bp1af58fjsnd4a08f796f4f",
-      //   "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-      // },
-    })
+    fetch(
+      `https://bayut.p.rapidapi.com/properties/detail?externalID=${externalID}`,
+      {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "47f6ed740fmsh71585dcfcf20c8bp1af58fjsnd4a08f796f4f",
+          "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+        },
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
-        setListingData(data[0]);
-        // console.log(data[0].photos);
-        const photosArray = data[0].photos;
-        // console.log(photosArray);
+        setListingData(data);
+        const photosArray = data.photos;
         let imgURLArray = [];
         for (const imgItem of photosArray) {
           imgURLArray.push(imgItem.url);
@@ -56,25 +40,44 @@ const Listing_Details = () => {
       });
   }, []);
 
-  console.log(listingData);
-  const amenities = listingData.amenities;
-  console.log(amenities);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => (prevCount + 1) % images.length);
+    }, 4000);
 
-  // const amenitiesItems = amenityItem2.map((item) => (
-  //   <p class="card-text">
-  //     <small class="text-muted">{item.text}</small>
-  //   </p>
-  // ));
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    setCurrentImage(images[count]);
+  }, [count]);
+
+  console.log(`Images Count: ${images.length}`);
 
   return (
-    <div id="listingDetails">
-      <h1>Explore the Details of Your Next Abode</h1>
-      <div class="card mb-3 listingDetailsCard">
-        <img src={currentImage}></img>{" "}
-        <div class="card-body">
-          <h5 class="card-title">{listingData.title_l2}</h5>
-          {/* {amenitiesItems} */}
-          <p class="card-text">{listingData.description_l2}</p>
+    <div className="listingDetailsContainer">
+      <div className="listingDetailsHeader">
+        <h1>
+          Embark on Your Journey to the perfect Abode. Uncover Every Detail!
+        </h1>
+      </div>
+      <div className="listingDetailsCard">
+        <div class="card mb-3">
+          {!currentImage ? (
+            <div>
+              <small> Loading ....</small>
+            </div>
+          ) : (
+            <img src={currentImage} class="card-img-top"></img>
+          )}
+          <div class="card-body">
+            <h5 class="card-title">{currentProperty.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">{currentProperty.amenities}</small>
+            </p>
+
+            {/* <p class="card-text">{description_l2}</p> */}
+          </div>
         </div>
       </div>
     </div>
