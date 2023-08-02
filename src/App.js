874954e77a673aee2131888994afc95cec53ceb5
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-02 16:12:06 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import BookingForm from "./Components/BookingForm";
 
 function App() {
@@ -8,6 +9,22 @@ function App() {
         
       </header>
       <BookingForm />
+=======
+import logo from "./logo.svg";
+import "./App.css";
+import RouterComponent from "./Components/RouterComponent";
+import NavBar from "./Components/NavBar";
+import Listings_Home from "./Components/Listings_Home";
+import Listing_Details from "./Components/Listing_Details";
+import FetchAPIData from "./Components/FetchAPIData";
+import Home from "./Components/Listings_Home";
+function App() {
+  return (
+    <div className="App">
+      <FetchAPIData />
+      <Listings_Home />
+      <RouterComponent />
+>>>>>>> 585c4bf1372b1542f7d8156d4fd141e9f2fcd1b3
     </div>
   );
 }
---
+++ src/Components/Listing_Booking.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Listing_Booking = () => {
+  return <div>Listing_Booking</div>;
+};
+
+export default Listing_Booking;
