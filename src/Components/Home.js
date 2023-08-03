52724d15975b11dcf047bb50aecd6f02f4a0b39b
mike <mjowiti@gmail.com>
2023-08-03 10:41:42 +0300

    adding functionality

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 
 import { useState, useEffect } from "react";
 
+
 function Home() {
   const [homes, setHomes] = useState([]);
   useEffect(() => {
@@ -8,21 +9,33 @@ function Home() {
       .then((resp) => resp.json())
       .then((homes) => setHomes(homes));
   }, []);
+  console.log(homes);
   const homesList = homes.map((home) => (
-    <div class="card" key={home.id}>
+    <div class="card" key={home.id} style={{width:"300px"}}>
       <img src={home.coverPhoto.url} class="card-img-top" alt={home.state} />
       <div class="card-body">
-        <h5 class="card-title">{home.location.name}</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <h5 class="card-title"> {home.location[2].name.toUpperCase()}, {home.location[3].name.toUpperCase()}</h5>
+        <p class="card-text">{home.title}</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">{home.price}</li>
-        <li class="list-group-item">{homes.rooms}</li>
-        <li class="list-group-item">{home.contactName}</li>
-        <li class="list-group-item">{home.area}</li>
+      <li class="list-group-item" style={{color:"blue",fontSize:"25px"}}>$ {home.price}</li>
+        <div style={{display:"flex",justifyContent:"center"}}>
+        <li class="list-group-item"> <i class='fa fa-bath'></i> {home.baths}</li>
+        <li class="list-group-item"> <i class='fa fa-bed'></i> {home.rooms}</li>
+        <li class="list-group-item"><i class="fa fa-expand"></i> {home.area} ft</li>
+       
+        </div>
+        <div style={{display:"flex",justifyContent:"space-between"}}>
+            <div style={{display:"flex" ,justifyContent:"space-between"}}>
+        <li class="list-group-item"><img style={{borderRadius:"50%",height:"30px",width:"30px"}}src={home.agency.logo.url}></img></li>
+        <li class="list-group-item" >{home.contactName}</li>
+        </div>
+        <div style={{display:"flex"}}>
+        <li class="list-group-item"><i class="fa fa-share-alt"></i></li>
+        <li class="list-group-item"><i class="fa fa-heart"></i></li>
+        <li class="list-group-item"><i class="fa fa-plus"></i></li>
+        </div>
+        </div>
       </ul>
       <div class="card-body">
         <a href="/" class="btn btn-primary">
