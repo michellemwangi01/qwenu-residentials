c708d98de2bda4f2c63866841a83f64c28143eaa
mike <mjowiti@gmail.com>
2023-08-03 13:50:37 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import RouterComponent from "./Components/RouterComponent";
 import NavBar from "./Components/NavBar";
 import FetchAPIData from "./Components/FetchAPIData";
-
+import Footer from "./Components/Footer";
 import { Outlet } from "react-router-dom";
 import Listing_Details from "./Components/Listing_Details";
+
 function App() {
   return (
     <div className="App">
       <FetchAPIData>
         <NavBar />
         <RouterComponent />
+        <Footer />
       </FetchAPIData>
     </div>
   );
